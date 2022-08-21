34fef50bceb0ba90f29e9d1b01685a1ae0c63d73
Olga <osa_007@mail.ru>
2022-08-21 21:24:53 +0300

    added task1 for dz4

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numA = NumberFromUser ("Введите число: ","Ошибка ввода!");
+int numN = NumberFromUser ("Введите степень числа: ", "Ошибка ввода!");
+int gradeNumber = GetGradeNumber(numA, numN);
+Console.WriteLine($"\t{numA} в степени {numN} -> {gradeNumber}");
+
+int NumberFromUser (string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetGradeNumber(int numberA, int numberN)
+{
+    int count = 1;
+    int gradeNum = 1;
+    while(count <= numberN)
+    {
+        gradeNum = gradeNum*numberA;
+        count++;
+    }
+    return gradeNum;
+}
