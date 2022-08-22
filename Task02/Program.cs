9ed344aa21d915172eee99ce787d5f05f83dae78
Olga <osa_007@mail.ru>
2022-08-22 21:24:51 +0300

    added task2 for dz4

---
+++ Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+Console.WriteLine($"Cумма цифр в числе {num} -> {GetSumDigits(num)}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSumDigits(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
